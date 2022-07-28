54342f9fbeccfba64129da13ce5b70b2cac86108
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2022-07-28 06:57:55 -0300

    condicionais

---
+++ condicionais.py
@@ -0,0 +1,47 @@
+print("Digite um nome")
+nome = input().lower().strip()
+
+if nome == "leandro":
+  print("Encontrei o nome procurado")
+  print("Encontrei o nome procurado")
+  print("Encontrei o nome procurado")
+  print("Encontrei o nome procurado")
+  print("Encontrei o nome procurado")
+elif nome == "danilo" or nome.find("danilo") != -1:
+  print("Encontrei o nome procurado")
+else:
+  print("Não encontrei o nome procurado")
+  print("Não encontrei o nome procurado")
+  print("Não encontrei o nome procurado")
+  print("Não encontrei o nome procurado")
+  print("Não encontrei o nome procurado")
+
+
+# # Java, Javascrip, C, C#
+# if(nome == "leandro"){
+#     print("Encontrei o nome procurado")
+# }
+# else{
+#     print("Não encontrei o nome procurado")
+# }
+
+# # Ruby
+# if nome == "leandro"
+#     print("Encontrei o nome procurado")
+# else
+#     print("Não encontrei o nome procurado")
+# end
+
+
+# Operadores condicionais
+
+# == # comparação
+# > # maior
+# < # menor
+# >= # maior ou igual
+# <= # menor ou iqual
+# != # diferente
+
+
+# or # ou
+# and # and
