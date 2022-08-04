844a38ec2bc99b12a511abfb9e2eaaf4772d5ba4
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2022-08-04 06:31:51 -0300

    exercicio

---
+++ dia11/exercicio2.py
@@ -0,0 +1,6 @@
+"""
+Utilizando o algoritimo criado no dia 10
+refatore o código fazendo a persistencia dos dados
+para que o cadastro de clientes, produtos e pedidos
+estejam persistidos no disco
+"""
