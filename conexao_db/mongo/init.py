69f183a8d735e3372e2c80ccd3abb9597c5eb040
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2022-08-09 06:18:17 -0300

    exercicio dia 16

--- conexao_db/mongo/init.py
+++ conexao_db/mongo/init.py
@@ -1,23 +1,31 @@
 # install
 # $ python -m pip install pymongo
+import os
+
 import pymongo
 
 print("========== connect ==============")
+# myclient = pymongo.MongoClient(os.getenv("DATABASE_URL"))
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-mydb = myclient["mydatabase"]
+mydb = myclient["desafio_21_dias_python"]
 
 print("========== use colection ==============")
-mycol = mydb["customers"]
+mycol = mydb["usuarios"]
 
 print("========== insert ==============")
-mydoc = { "name": "John", "address": "Highway 37" }
+mydoc = { 
+  "nome": "danilo",
+  "email": "danilo@torneseumprogramador.com.br",
+  "endereco": "Rua teste 123"
+}
+
 mycol.insert_one(mydoc)
 
 docs = [
-    { "name": "John1", "address": "Highway 38" },
-    { "name": "John2", "address": "Highway 39" },
-    { "name": "John3", "address": "Highway 40" },
-    { "name": "John4", "address": "Highway 41" },
+    { "nome": "John1", "endereco": "Highway 38" },
+    { "nome": "John2", "endereco": "Highway 39" },
+    { "nome": "John3", "endereco": "Highway 40" },
+    { "nome": "John4", "endereco": "Highway 41" },
 ]
 mycol.insert_many(docs)
 
@@ -27,7 +35,7 @@ one = mycol.find_one()
 print(one)
 
 print("========== filter ==============")
-myquery = { "address": "Park Lane 38" }
+myquery = { "endereco": "Rua teste 123" }
 docs = mycol.find(myquery)
 for doc in docs:
   print(doc)
@@ -38,24 +46,24 @@ for doc in docs:
   print(doc)
 
 print("========== sort ==============")
-docs = mycol.find().sort("name")
+docs = mycol.find().sort("nome")
 for doc in docs:
   print(doc)
 
 print("========== sort desc ==============")
-docs = mycol.find().sort([("name", pymongo.DESCENDING)])
+docs = mycol.find().sort([("nome", pymongo.DESCENDING)])
 for doc in docs:
   print(doc)
 
 print("========== update ==============")
-myquery = { "address": "Valley 345" }
-newvalues = { "$set": { "address": "Canyon 123" } }
+myquery = { "endereco": "Valley 345" }
+newvalues = { "$set": { "endereco": "Canyon 123" } }
 mycol.update_one(myquery, newvalues)
 for doc in mycol.find():
   print(doc)
 
 print("========== delete ==============")
-myquery = { "address": "Mountain 21" }
+myquery = { "endereco": "Mountain 21" }
 mycol.delete_one(myquery)
 
 
---
+++ dia16/exercicio.py
@@ -0,0 +1,37 @@
+''''
+Faca um programa para calcular os valores de um pedido
+para isso crie uma classe de pedido que tenha relacao com uma classe de cliente
+
+pedido = {
+    "id": 1,
+    "cliente": {
+        "nome": "Walter"
+    },
+    "itens": []
+}
+
+nesse pedido, coloque uma propriedade de itens, contendo instancias de uma classe de produto
+no pedido, crie um método na classe de pedido para calcular o valor total.
+
+Na usabilidade do programa, crie um relatório mostrando da seguinte forma:
+----------------------------------------------------------------
+Pedido Id: 1
+Nome: João
+Valor Total: R$ 999,99
+----------------------------------------------------------------
+O que terá na classe de pedido:
+- id - Autoincrement
+- cliente
+- itens []
+O que terá na classe cliente:
+- id - Autoincrement
+- Nome
+- email
+O que terá na classe produto:
+- id - Autoincrement
+- Nome
+- descrição
+- preço
+
+Utilize um banco de dados profissional para armazenar as informações
+'''
