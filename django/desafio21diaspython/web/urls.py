d2d7a4d4f3e22f8a7743c03d50e0269513da791a
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2022-08-11 06:05:15 -0300

    configurando rotas, templates e arquivos estaticos no django

---
+++ django/desafio21diaspython/web/urls.py
@@ -0,0 +1,12 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index),
+    path('html', views.html_bruto),
+    path('json', views.json),
+    path('xml', views.xml_bruto),
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
---
+++ django/desafio21diaspython/web/views.py
@@ -0,0 +1,20 @@
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render
+
+
+def index(request):
+    resposta = {}
+    resposta["conteudo"] = "Estou passando uma chave chamado conteudo para meu template"
+    return render(request, 'home/index.html', resposta)
+
+def html_bruto(request):
+    return HttpResponse("<h1>respondendo por html</h1>")
+
+def json(request):
+    resposta = {}
+    resposta["conteudo"] = "Agora com API JSON"
+    return JsonResponse(resposta)
+
+def xml_bruto(request):
+    xml_bruto = '<?xml version="1.0" encoding="UTF-8"?><bookstore><book category="cooking"><title lang="en">Everyday Italian</title><author>Giada De Laurentiis</author><year>2005</year><price>30.00</price></book></bookstore>'
+    return HttpResponse(xml_bruto, content_type="text/xml")
